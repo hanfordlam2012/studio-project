1a6aead9be900836b937ce2b182981d87933aeaf
HL <hanfordlam2012@hotmail.com>
2022-06-13 16:27:07 +1000

    new student

--- models/FeatureScripts.js
+++ models/FeatureScripts.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs')
 // hash password feature for password resets
 hashPassword = async function () {
     let salt = bcrypt.genSaltSync(10)
-    let hashedPassword = bcrypt.hashSync('itan2022', salt)
+    let hashedPassword = bcrypt.hashSync('chengyu', salt)
     console.log(hashedPassword)
 }
 
