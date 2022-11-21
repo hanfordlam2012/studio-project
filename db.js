5767307070843bd18e7100cbb30aff4053f4228c
HL <hanfordlam2012@hotmail.com>
2022-11-21 18:23:39 +1100

    dynamic port

--- db.js
+++ db.js
@@ -6,5 +6,10 @@ const client = new MongoClient(process.env.URI, { useNewUrlParser: true, useUnif
 client.connect(err => {
   module.exports = client;
   const app = require('./app');
-  app.listen(process.env.PORT || 3000)
+  // dynamic port selection
+  const server = app.listen(0, () => {
+    console.log('Studio app listening at http://localhost:', server.address().port);
+});
+// old code does not allow dynamic port
+// (process.env.PORT || 3000)
 });
