3b7d92605c8c9daeda5a406f37f2cd3985aee56e
HL <hanfordlam2012@hotmail.com>
2022-11-30 13:54:15 +1100

    leaderboard and home

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ dotenv.config()
 const MongoClient = require('mongodb').MongoClient;
 
 const client = new MongoClient(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 client.connect(err => {
   module.exports = client;
   const app = require('./app');
