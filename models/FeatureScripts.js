c9c4eeadfcf4346336ab9147b55bab77f4f97f76
HL <hanfordlam2012@hotmail.com>
2023-01-22 14:15:02 +1100

    black backgrounds schedule and parents

--- models/FeatureScripts.js
+++ models/FeatureScripts.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs')
 // hash password feature for password resets
 hashPassword = async function () {
     let salt = bcrypt.genSaltSync(10)
-    let hashedPassword = bcrypt.hashSync('chengyu', salt)
+    let hashedPassword = bcrypt.hashSync('enylecoj', salt)
     console.log(hashedPassword)
 }
 
