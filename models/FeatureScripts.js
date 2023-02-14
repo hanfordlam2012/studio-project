716e5a5d87bedcc6af98c034a589a82650002760
HL <hanfordlam2012@hotmail.com>
2023-02-14 22:16:43 +1100

    rubric update

--- models/FeatureScripts.js
+++ models/FeatureScripts.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs')
 // hash password feature for password resets
 hashPassword = async function () {
     let salt = bcrypt.genSaltSync(10)
-    let hashedPassword = bcrypt.hashSync('enylecoj', salt)
+    let hashedPassword = bcrypt.hashSync('evan28jan', salt)
     console.log(hashedPassword)
 }
 
