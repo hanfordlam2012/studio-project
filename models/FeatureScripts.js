9df76a313354333e903acf19015d9d14a03f4a29
HL <hanfordlam2012@hotmail.com>
2023-04-02 16:06:33 +1000

    2024 and apirl missions

--- models/FeatureScripts.js
+++ models/FeatureScripts.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs')
 // hash password feature for password resets
 hashPassword = async function () {
     let salt = bcrypt.genSaltSync(10)
-    let hashedPassword = bcrypt.hashSync('evan28jan', salt)
+    let hashedPassword = bcrypt.hashSync('austinpiano2023', salt)
     console.log(hashedPassword)
 }
 
