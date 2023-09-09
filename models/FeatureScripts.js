ba6335940d97a1e24d472c59b29f27f1e309d11b
HL <hanfordlam2012@hotmail.com>
2023-09-09 13:40:09 +1000

    but will i

--- models/FeatureScripts.js
+++ models/FeatureScripts.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs')
 // hash password feature for password resets
 hashPassword = async function () {
     let salt = bcrypt.genSaltSync(10)
-    let hashedPassword = bcrypt.hashSync('', salt)
+    let hashedPassword = bcrypt.hashSync('lucas', salt)
     console.log(hashedPassword)
 }
 
