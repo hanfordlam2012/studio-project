865f9fac9af2e5a18452f4aec6d391a31017316c
HL <hanfordlam2012@hotmail.com>
2024-03-23 18:33:26 +1100

    focus higher levels

--- models/FeatureScripts.js
+++ models/FeatureScripts.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs')
 // hash password feature for password resets
 hashPassword = async function () {
     let salt = bcrypt.genSaltSync(10)
-    let hashedPassword = bcrypt.hashSync('connie2024', salt)
+    let hashedPassword = bcrypt.hashSync('nicole2024', salt)
     console.log(hashedPassword)
 }
 
