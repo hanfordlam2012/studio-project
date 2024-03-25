dca2559480b6fcd3274986274ead811d94c46da2
HL <hanfordlam2012@hotmail.com>
2024-03-25 15:12:44 +1100

    theory course

--- router.js
+++ router.js
@@ -38,7 +38,7 @@ router.post('/isCorrect', userController.isCorrect)
 // subscription routes
 router.post('/create-checkout-session', userController.createCheckoutSession)
 router.post('/create-portal-session', userController.createPortalSession)
-router.get('/shop', userController.showShopPage)
+router.get('/theory', userController.showShopPage)
 router.get('/success', userController.showSuccessPage)
 router.get('/tutorials', userController.mustBeLoggedIn, userController.showTutorialsPage)
 
