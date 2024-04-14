d97a3a6aba82f7431f7b1dc087af17fbbbe9bbed
HL <hanfordlam2012@hotmail.com>
2024-04-14 16:54:30 +1000

    emojis

--- models/FeatureScripts.js
+++ models/FeatureScripts.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs')
 // hash password feature for password resets
 hashPassword = async function () {
     let salt = bcrypt.genSaltSync(10)
-    let hashedPassword = bcrypt.hashSync('nicole2024', salt)
+    let hashedPassword = bcrypt.hashSync('2024yuting', salt)
     console.log(hashedPassword)
 }
 
